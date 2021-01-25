dda1c54b96ece40fa695fc038b8a3743111fb4b7
noobless <nuzulzen@gmail,com>
2021-01-25 17:43:26 +0700

    add Global styled

---
+++ src/components/styles/GlobalStyled.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyled = createGlobalStyle`
+ * {
+     font-family: 'Roboto' , sans-serif;
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+ }
+`
